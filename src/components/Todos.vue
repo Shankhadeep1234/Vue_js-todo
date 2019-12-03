e79fdd6b5db6c26a69f01d37d47f5626d7a3d648
Shankhadeep Bhadra <shankhadeepraj@gmail.com>
2019-12-03 09:44:45 +0530

    methods completed

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
     <div v-for="todo in todos" :key="todo.id">
-      <h3>{{ todo.title }}</h3>
+      <TodoItem v-bind:todo="todo" />
     </div>
   </div>
 </template>
 
 <script>
+import TodoItem from "./TodoItem";
 export default {
   name: "Todos",
+  components: { TodoItem },
   props: ["todos"]
 };
 </script>
